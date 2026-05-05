12d1ee93dd4c21ab75b22ac02aaf054d3662a866
blafed <blafed@fake>
2026-05-05 17:06:30 +0300

    tok+sym+lex

--- jo.h
+++ jo.h
@@ -27,24 +27,19 @@ size_t jo_io_write(int fd, const void *buf, size_t n);
 long jo_io_seek(int fd, long offset, int origin);
 void jo_exec(const char *cmd);
 
-enum TokenType : char {
+enum jo_token_type : char {
   JO_TOKEN_NONE,
-  JO_TOKEN_ID,  // sequence of alphabet (multiple)
-  JO_TOKEN_NUM, // sequence of digits (multiple)
-  JO_TOKEN_SYM, // any symbol single only (single)
-  JO_TOKEN_SEP, // sequence of whitespace of same type, flattened (single)
+  JO_TOKEN_ID,    // sequence of alphabet (multiple)
+  JO_TOKEN_NUM,   // sequence of digits (multiple)
+  JO_TOKEN_SYM,   // any symbol single only (single) (multiple when meaning)
+  JO_TOKEN_SEP,   // sequence of whitespace of same type, flattened (single)
+  JO_TOKEN_EMPTY, // deleted token
 
   // phase2
-  JO_TOKEN_INTERP, // begins with $ inside string
-  JO_TOKEN_DOLLAR, // begins with $ outside string
   JO_TOKEN_ROOT,
-  JO_TOKEN_KEY,
+  JO_TOKEN_INTERP, // begins with $ inside string
   JO_TOKEN_GROUP,  //{} [] () <>
-  JO_TOKEN_CURRY,  // if x
-  JO_TOKEN_BLOCK,  //{
-  JO_TOKEN_BOX,    //[
-  JO_TOKEN_PAREN,  //(
-  JO_TOKEN_ESCAPE, // temporary
+  JO_TOKEN_ESCAPE, //
   JO_TOKEN_STR1,   // '
   JO_TOKEN_STR2,   // "
   JO_TOKEN_SLUG,
@@ -94,8 +89,13 @@ static const char JO_CHAR_PAIR[] = {
     ['}'] = '{', [')'] = '(', ['>'] = '<', [']'] = '[',
 };
 
+static const unsigned char JO_CHAR_ESCAPE[256] = {
+    ['\''] = '\'', ['"'] = '"',  ['\\'] = '\\', ['n'] = '\n',
+    ['r'] = '\r',  ['t'] = '\t', ['b'] = '\b',  ['f'] = '\f',
+    ['v'] = '\v',  ['a'] = '\a', ['0'] = '\0'};
+
 // clang-format off
-static const char *token_type_name(enum TokenType t) {
+static const char *token_type_name(enum jo_token_type t) {
   switch (t) {
   case JO_TOKEN_NONE:     return "NONE";
   case JO_TOKEN_ID:       return "ID";
@@ -105,11 +105,6 @@ static const char *token_type_name(enum TokenType t) {
 
   case JO_TOKEN_GROUP:    return "GROUP";
   case JO_TOKEN_INTERP:   return "INTERP";
-  case JO_TOKEN_ROOT:     return "ROOT";
-  case JO_TOKEN_KEY:      return "KEY";
-  case JO_TOKEN_BLOCK:    return "BLOCK";
-  case JO_TOKEN_BOX:      return "BOX";
-  case JO_TOKEN_PAREN:    return "PAREN";
   case JO_TOKEN_ESCAPE:   return "ESC";
   case JO_TOKEN_STR1:     return "STR";
   case JO_TOKEN_STR2:     return "STR2";
@@ -230,7 +225,10 @@ struct jo_ast {};
 struct jo_token {
   uint8_t type, data;
   int pos, line, col, len;
-  const char *val;
+  union {
+    const char *val;
+    uintptr_t ref;
+  };
   int depth;
 };
 struct jo_cursor {
@@ -242,7 +240,7 @@ typedef struct jo_ast jo_ast;
 typedef struct jo_cursor jo_cursor;
 
 // clang-format off
-// the text SHOULD be null terminated
+// the src should be NULL TERMINATED
 int jo_tok(const char *src, struct jo_cursor *tok, struct jo_token *out) {
   out->pos = tok->pos; out->line = tok->line; out->col = tok->col;
   int start = tok->pos;
@@ -288,9 +286,320 @@ int jo_tok(const char *src, struct jo_cursor *tok, struct jo_token *out) {
 
   return 0;
 }
+
+
+static inline enum Symbol jo_sym(const char* c, size_t len) {
+    switch (len) {
+
+		//TODO remove this?
+    case 1:
+        switch (c[0]) {
+            case '!': return SYM_EXCL;
+            case '"': return SYM_DQUOTE;
+            case '#': return SYM_HASH;
+            case '$': return SYM_DOLLAR;
+            case '%': return SYM_PERCENT;
+            case '&': return SYM_AMP;
+            case '\'': return SYM_SQUOTE;
+            case '(': return SYM_LPAREN;
+            case ')': return SYM_RPAREN;
+            case '*': return SYM_STAR;
+            case '+': return SYM_PLUS;
+            case ',': return SYM_COMMA;
+            case '-': return SYM_MINUS;
+            case '.': return SYM_DOT;
+            case '/': return SYM_SLASH;
+            case ':': return SYM_COLON;
+            case ';': return SYM_SEMI;
+            case '<': return SYM_LT;
+            case '=': return SYM_EQ;
+            case '>': return SYM_GT;
+            case '?': return SYM_QUESTION;
+            case '@': return SYM_AT;
+            case '[': return SYM_LBRACKET;
+            case '\\': return SYM_BACKSLASH;
+            case ']': return SYM_RBRACKET;
+            case '^': return SYM_CARET;
+            case '_': return SYM_UNDERSCORE;
+            case '`': return SYM_BACKTICK;
+            case '{': return SYM_LBRACE;
+            case '|': return SYM_PIPE;
+            case '}': return SYM_RBRACE;
+            case '~': return SYM_TILDE;
+        }
+        break;
+
+    case 2:
+        switch (c[0]) {
+            case ':': if (c[1] == '=') return SYM_DECL; if(c[1] == ':') return SYM_SCOPE; break;
+            case '?': if (c[1] == '?') return SYM_NULLER; break;
+            case '+': if (c[1] == '+') return SYM_INC;
+                      if (c[1] == '=') return SYM_ADD_EQ;
+                      break;
+            case '-': if (c[1] == '-') return SYM_DEC;
+                      if (c[1] == '=') return SYM_SUB_EQ;
+                      if (c[1] == '>') return SYM_ARROW;
+                      break;
+            case '<': if (c[1] == '<') return SYM_SHL;
+                      if (c[1] == '=') return SYM_LTE;
+                      break;
+            case '>': if (c[1] == '>') return SYM_SHR;
+                      if (c[1] == '=') return SYM_GTE;
+                      break;
+            case '|': if (c[1] == '|') return SYM_LOR;
+                      if (c[1] == '=') return SYM_OR_EQ;
+                      break;
+            case '&': if (c[1] == '&') return SYM_LAND;
+                      if (c[1] == '=') return SYM_AND_EQ;
+                      break;
+            case '.': if (c[1] == '.') return SYM_DOTDOT; break;
+            case '*': if(c[1] == '/') return SYM_COMMENT2_END; if (c[1] == '=') return SYM_MUL_EQ; break;
+        	case '/':
+        	    if (c[1] == '=') return SYM_DIV_EQ;
+        	    if (c[1] == '/') return SYM_COMMENT1;
+        	    if (c[1] == '*') return SYM_COMMENT2_BEGIN;
+        	    break;
+            case '%': if (c[1] == '=') return SYM_MOD_EQ; break;
+            case '^': if (c[1] == '=') return SYM_XOR_EQ; break;
+            case '=': if (c[1] == '=') return SYM_EQEQ;
+                      if (c[1] == '>') return SYM_FATARROW;
+                      break;
+            case '!': if (c[1] == '=') return SYM_NEQ; break;
+        }
+        break;
+
+    case 3:
+        switch (c[0]) {
+			case '&': if (c[1] == '=' && c[2] == '=') return SYM_REF_EQ; break;
+            case '<': if (c[1] == '<' && c[2] == '=') return SYM_SHL_EQ; break;
+            case '>': if (c[1] == '>' && c[2] == '=') return SYM_SHR_EQ; break;
+            case '"': if (c[1] == '"' && c[2] == '"') return SYM_TRIPLE_DQUOTE; break;
+            case '\'': if (c[1] == '\'' && c[2] == '\'') return SYM_TRIPLE_SQUOTE; break;
+			case '?': if (c[1] == '?' && c[2] == '?') return SYM_TODO; break;
+        }
+        break;
+    }
+
+	return 0;
+}
+
+void jo_tok_sym(jo_token* tokens) {
+	jo_token *stack[3];
+	int sp = 0;
+
+	jo_token *t = tokens;
+	
+	for(;; ++t) {
+		int type = t->type;
+
+		if(!type) break;
+		else if(type == JO_TOKEN_SYM) stack[sp++] = t;
+		else if(sp < 2) sp = 0;
+
+
+		if(sp == 3) {
+			unsigned char data = jo_sym(stack[0]->val, 3);
+			if(data) {
+				stack[0]->data = data;
+				stack[0]->len = 3;
+				stack[1]->type = JO_TOKEN_EMPTY;
+				stack[2]->type = JO_TOKEN_EMPTY;
+				sp = 0;
+			} else {
+				data = jo_sym(stack[0]->val, 2);
+				if(data) {
+					stack[0]->data = data;
+					stack[0]->len = 2;
+					stack[1]->type = JO_TOKEN_EMPTY;
+
+					stack[0] = stack[2];
+					sp = 1;
+				} else {
+					stack[0] = stack[1];
+					stack[1] = stack[2];
+					sp = 2;
+				}
+			}
+		} else if(sp == 2 && type != JO_TOKEN_SYM) {
+			unsigned char data = jo_sym(stack[0]->val, 2);
+			if(data) {
+				stack[0]->data = data;
+				stack[0]->len = 2;
+				stack[1]->type = JO_TOKEN_EMPTY;
+			}
+			sp = 0;
+		}
+	}
+}
+
+static jo_token JO_ROOT_TOKEN = {.type = JO_TOKEN_ROOT};
+
+void jo_lex(jo_token* tokens, jo_token* stack[]) {
+
+  #define in_string (cur->type == JO_TOKEN_STR1 || cur->type == JO_TOKEN_STR2)
+	#define in_comment (cur->type == JO_TOKEN_COMMENT1 || cur->type == JO_TOKEN_COMMENT2)
+	#define in_slug (in_string || in_comment)
+	#define in_atom (cur->type == JO_TOKEN_ID || cur->type == JO_TOKEN_NUM)
+	#define in_escape (cur->type == JO_TOKEN_ESCAPE)
+	#define in_interp (cur->type == JO_TOKEN_INTERP)
+	// #define in_dollar (cur->type == jo_token_interp)
+
+  enum {_NOTHING, _PUSH, _POP, _UPGRADE, _KILL, _APPEND, _TERM, _SLUGY, _REMOVE, _ERR };
+  #define kill todo = _KILL;
+	#define append todo = _APPEND;
+	#define term todo = _TERM;
+	#define slugy todo = _SLUGY;
+	#define remove todo = _REMOVE;
+	#define err todo = _ERR;
+	#define push(x) todo = _PUSH, payload = x;
+	#define pop todo = _POP;
+	#define upgrade(x) todo = _UPGRADE, payload = x;
+  #define escape(type, val) {upgrade(type) t->data = val;}
+  #define keep ;
+
+	jo_token* t = tokens;
+	int sp = 0;
+  stack[sp] = &JO_ROOT_TOKEN;
+  
+  for(;; t++) {
+    int type = t->type;
+    if (!type) break;
+    // printf("sp %d, type %d %zu\n\n", sp, type, t - tokens);
+    jo_token * cur = stack[sp];
+    t->depth = cur->depth + 1;
+
+    int todo = _NOTHING;
+    enum jo_token_type payload;
+    switch (t->type) {
+      case JO_TOKEN_EMPTY: continue;
+      case JO_TOKEN_ID:
+      case JO_TOKEN_NUM:
+        if (in_atom) append
+        else if(in_slug) slugy
+        else push(t->type)
+      case JO_TOKEN_SEP:
+        switch (t->val[0]) {
+          case '\n':
+            if (cur->type == JO_TOKEN_COMMENT1) pop
+            else if(in_atom) term
+            // else if(in_angle) kill
+            else if(in_slug) slugy
+            else if(in_interp) term
+            else keep
+          break;
+          default:
+            if(in_atom) term
+            else keep
+            // else if(in_angle)
+            break;
+        }
+        break;
+      case JO_TOKEN_SYM:
+        switch (t->data) {
+          case '.':
+          case '_':
+            if (in_atom) append
+            else if(in_slug) slugy
+            else push(JO_TOKEN_ID)
+            break;
+          case '{': case '[': case '(':
+            if (in_atom) term
+            else if(in_interp) push(JO_TOKEN_GROUP)
+            else if(in_slug) slugy
+            else upgrade(JO_TOKEN_GROUP)
+            break;
+          case '}': case ']': case ')':
+            if(in_atom) term
+            else if(cur->type == JO_TOKEN_GROUP && t->data == JO_CHAR_PAIR[cur->data]) pop
+            else if(in_slug) slugy
+            else err
+            break;
+          case '$':
+            if(in_atom)term
+            else if(in_escape) escape(JO_TOKEN_SLUG, JO_CHAR_ESCAPE[t->data])
+            else if(in_comment) append 
+            else push(JO_TOKEN_INTERP)
+            break;
+          case SYM_COMMENT1:
+            if(in_atom) term
+            else if (in_slug) slugy
+            else push(JO_TOKEN_COMMENT1)
+            break;
+          case SYM_COMMENT2_BEGIN:
+            if(in_atom) term
+            else if (in_slug) slugy
+            else push(JO_TOKEN_COMMENT2)
+            break;
+          case SYM_COMMENT2_END:
+            if(in_atom) term
+            else if(in_comment && cur->type == JO_TOKEN_COMMENT2) pop
+            else if(in_slug) slugy
+            else err
+            break;
+          case '\\':
+            if(in_atom) term
+            else if(in_escape) escape(JO_TOKEN_SLUG, JO_CHAR_ESCAPE[t->data])
+            else upgrade(JO_TOKEN_ESCAPE)
+            break;
+          case '\'':
+            if(in_atom) term
+            else if(cur->type == JO_TOKEN_STR1) pop
+            else if(in_escape) escape(JO_TOKEN_SLUG, JO_CHAR_ESCAPE[t->data])
+            else push(JO_TOKEN_STR1)
+            break;
+          case '"':
+            if(in_atom) term
+            else if(cur->type == JO_TOKEN_STR2) pop
+            else if(in_escape) escape(JO_TOKEN_SLUG, JO_CHAR_ESCAPE[t->data])\
+            else push(JO_TOKEN_STR1)
+            break;
+          default:
+            if(in_atom) term
+            else if(in_escape) escape(JO_TOKEN_SLUG, JO_CHAR_ESCAPE[t->data])
+            else if(in_slug) slugy
+            else keep
+            break;
+        }
+        break;
+    }
+
+    switch(todo) {
+      case _APPEND:cur->len += t->len;t->type = 0;break;
+      case _PUSH:stack[++sp]=t;t->type=payload;break;
+      case _UPGRADE:t->type=payload;stack[sp]=t; break;
+      case _POP:sp--;t->type = 0; break;
+      case _SLUGY:t->type=JO_TOKEN_SLUG;break;
+      case _REMOVE:t->type=0;break;
+      case _TERM:t--;sp--;break;
+      case _ERR: jo_echo("error parsing \n", 14); break;
+    }
+  }
+#undef in_string
+#undef in_comment
+#undef in_slug
+#undef in_atom
+#undef in_escape
+#undef in_interp
+
+#undef kill
+#undef append
+#undef term
+#undef slugy
+#undef remove
+#undef err
+#undef push
+#undef pop
+#undef upgrade
+#undef escape
+#undef keep
+}
+
+// void jo_parse(jo_token* tokens) {}
+
 // clang-format on
 
 // clang-format off
+
 
 //
 //=======================================
@@ -321,11 +630,22 @@ void print_token(const struct jo_token *t) {
 }
 
 int main() {
-  const char *input = "\b\n";
+  const char *input = "hi there {me is soo}";
   jo_cursor cursor = {};
   jo_token token = {};
-  while (jo_tok(input, &cursor, &token))
-    print_token(&token);
+  jo_token tokens[1000];
+  jo_token *stack[1000];
+  int i = 0;
+
+  while (jo_tok(input, &cursor, &tokens[i++]))
+    ;
+  jo_tok_sym(tokens);
+  jo_lex(tokens, stack);
+  for (int j = 0; j < i; j++) {
+    print_token(&tokens[j]);
+  }
   printf("\n");
   return 0;
 }
+
+void jo_echo(const char *msg, size_t len) { printf("%.*s", (int)len, msg); }
